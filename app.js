078543a5b6f807bf92fbd63207dd8f69bd3f06a9
Dan Silver <dannysilver3@gmail.com>
2012-12-22 15:47:11 -0600

    Posts link to their own page

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/post/:title', routes.post);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-
-/*
- * GET home page.
- */
 var db = require('../db')
 exports.index = function(req, res){
 	db.posts.findAll().success(function(posts) {
 		res.render('index', { title: 'Dan\'s Blog', posts: posts});
+	});
+};
+
+exports.post = function(req, res){
+	console.log('A');
+	db.posts.find({where: {title:req.params.title}}).success(function(post) {
+		console.log('B');
+		res.render('post', { title: 'Dan\'s Blog', post: post});
 	});
 };
