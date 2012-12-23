87ef19bc3c00a0f68b3aaa7930531c562cbf712a
Dan Silver <dannysilver3@gmail.com>
2012-12-23 15:38:51 -0600

    Google OpenID added, removed user db

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var db = require('../db')
 exports.index = function(req, res){
+	if (req.user) {
+	//	console.log(req.user);
+		console.log('User is logged in as '+req.user.displayName);
+	}
 	db.posts.findAll().success(function(posts) {
-		res.render('index', { title: 'Dan\'s Blog', posts: posts});
+		res.render('index', { title: 'Dan\'s Blog', posts: posts, user: req.user});
 	});
 };
 
 exports.post = function(req, res){
-	console.log('A');
 	db.posts.find({where: {title:req.params.title}}).success(function(post) {
-		console.log('B');
 		res.render('post', { title: 'Dan\'s Blog', post: post});
 	});
 };
