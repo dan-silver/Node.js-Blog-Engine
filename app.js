4c883bea69318cb5999f0631aa172856026ff445
Dan Silver <dannysilver3@gmail.com>
2012-12-23 02:28:00 -0600

    Added passport with openID and a Google endpoint

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , db = require('./db')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
 
 var app = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -21,6 +31,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -30,8 +42,42 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
 app.get('/', routes.index);
 app.get('/post/:title', routes.post);
+
+app.get('/auth/google', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/auth/google/return', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
