11592ff00be6df31d971e1e238822b1f97f1e648
Dan Silver <dannysilver3@gmail.com>
2013-01-17 02:05:38 -0800

    Display posted dates on blog entries

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var db = require('../db')
+var moment = require('moment');
+
 exports.index = function(req, res){
 	db.posts.findAll({order: 'createdAt DESC'}).success(function(posts) {
+		posts.forEach(function(post) {
+			post.createdAt = moment(post.createdAt).format("MMMM Do YYYY");
+		});
 		res.render('index', {posts: posts, noTitleLink: false});
 	});
 };
 
 exports.post = function(req, res) {
 	db.posts.find({where: {title:req.params.title}}).success(function(post) {
+		post.createdAt = moment(post.createdAt).format("MMMM Do YYYY");
 		res.render('post', {post: post, noTitleLink: true});
 	});
 };
