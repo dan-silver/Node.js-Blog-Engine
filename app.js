e0ab811ce061ced1bb3e8369de08b718d525bf16
Dan Silver <dannysilver3@gmail.com>
2013-01-21 21:39:41 -0800

    Db settings passed through module

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var blog = require('blog');
+var blog = require('blog')
+  , config = require('./config');
+
 blog.start({
 	title: "Dan Silver's Blog",
 	adminGoogleEmail: 'dannysilver3@gmail.com',
 	port: 3000,
-	liveDomain: 'http://dan-silver.herokuapp.com'
+	liveDomain: 'http://dan-silver.herokuapp.com',
+	database: {
+		database: config.database || process.env.database,
+		user: config.user || process.env.user,
+		password: config.password || process.env.password,
+		host: config.host || process.env.host,
+		dbPort: config.dbPort || process.env.dbPort
+	}
 });
